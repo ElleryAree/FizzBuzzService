395dfbb69acf90c73381dea103ebacaeaf44e7a7
Alexey Copylov <ElleryAree@gmail.com>
2018-10-06 21:18:32 +0300

    Basic logger

--- src/main/java/fizz/controller/FizzController.java
+++ src/main/java/fizz/controller/FizzController.java
@@ -2,6 +2,8 @@ package fizz.controller;
 
 import fizz.model.FizzBuzzResponse;
 import fizz.service.FizzBuzzService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FizzController {
     private final FizzBuzzService fizzBuzzService;
+    private static final Logger logger = LogManager.getLogger(FizzController.class);
 
     @Autowired
     public FizzController(FizzBuzzService fizzBuzzService) {
@@ -20,6 +23,8 @@ public class FizzController {
 
     @RequestMapping("/fizz")
     public FizzBuzzResponse getFizz(@RequestParam(value="number") int number) {
+        logger.info(() -> "Recieved request for number " + number);
+
         return new FizzBuzzResponse(number, fizzBuzzService.getFizzBuzz(number));
     }
 }
